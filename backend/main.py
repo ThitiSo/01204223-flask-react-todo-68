65de8d2cf834b69391995b02b547cc394547ad20
ThitiSo <thitis14443@gmail.com>
2026-01-23 07:24:17 +0700

    migration

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column
+from flask_migrate import Migrate                        # import library
 
 app = Flask(__name__)
 CORS(app)
@@ -13,6 +15,7 @@ class Base(DeclarativeBase):
   pass
 
 db = SQLAlchemy(app, model_class=Base)
+migrate = Migrate(app, db) 
 
 class TodoItem(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
@@ -25,8 +28,8 @@ class TodoItem(db.Model):
             "title": self.title,
             "done": self.done
         }
-with app.app_context():
-    db.create_all()
+# with app.app_context():
+#     db.create_all()
 
 
 INITIAL_TODOS = [
