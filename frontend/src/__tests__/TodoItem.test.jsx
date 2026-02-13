d8d7ec609632e87c12c4f41469e9af0167920ae5
ThitiSo <thitis14443@gmail.com>
2026-02-13 15:53:11 +0700

    APP TEST FIX!

--- frontend/src/__tests__/TodoItem.test.jsx
+++ frontend/src/__tests__/TodoItem.test.jsx
@@ -1,6 +1,11 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'   // เพิ่ม *** fireEvent
+import userEvent from '@testing-library/user-event'
+
 import { expect } from 'vitest'
 import TodoItem from '../ToDoItem.jsx'
+
+
+
 const baseTodo = {             // ** TodoItem พื้นฐานสำหรับทดสอบ
   id: 1,
   title: 'Sample Todo',
@@ -46,4 +51,52 @@ it('renders with comments correctly', () => {
     // ... ละตอนต้นไว้
      expect(screen.getByText(/2/)).toBeInTheDocument();
   });
+  it('makes callback to toggleDone when Toggle button is clicked', () => {
+    const onToggleDone = vi.fn();
+    render(
+      <TodoItem 
+       todo={baseTodo} 
+       toggleDone={onToggleDone} />
+    );
+    const button = screen.getByRole('button', { name: /toggle/i });
+    button.click();
+    expect(onToggleDone).toHaveBeenCalledWith(baseTodo.id);
+  });
+    it('makes callback to deleteTodo when delete button is clicked', () => {
+        const onToggleDelete = vi.fn();
+            render(
+      <TodoItem 
+       todo={baseTodo} 
+       deleteTodo={onToggleDelete} />
+    );
+        const button = screen.getByRole('button', { name: /❌/i });
+    button.click();
+    expect(onToggleDelete).toHaveBeenCalledTimes(1);
+  expect(onToggleDelete).toHaveBeenCalledWith(baseTodo.id);
+    //
+    // *** TODO: เขียนเอง
+    //
+  });
+   it('makes callback to addNewComment when a new comment is added', async () => {
+    const onAddNewComment = vi.fn();
+    render(
+<TodoItem 
+      todo={baseTodo} 
+      addNewComment={onAddNewComment} 
+    />
+    );
+
+    // พิมพ์ข้อความลงใน textbox
+    const input = screen.getByRole('textbox');
+    await userEvent.type(input, 'New comment');
+
+    // กดปุ่ม: ในที่นี้เราใช้ fireEvent เพราะว่าระหว่างการอัพเดทจะมีการเปลี่ยน state ถ้าไม่ใช่จะมี warning
+    const button = screen.getByRole('button', { name: /add comment/i });
+    fireEvent.click(button);
+
+    // assert
+    expect(onAddNewComment).toHaveBeenCalledWith(baseTodo.id, 'New comment');
+  });
 });
+
+
